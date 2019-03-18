70fe9c247a37c6dd606c560fe2c2ed5b88af782d
grant smith <grantsmith1256@gmail.com>
2019-03-18 18:18:15 +0000

    updated and finished

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -5,13 +5,16 @@ class UserController < ApplicationController
   end
 
   post '/account_creation' do
-    @user = User.create(email: params[:user][:email], password: params[:user][:password])
-    session[:id] = @user.id
-    redirect '/account'
+    if !User.all.find_by(email: params[:user][:email])
+      @user = User.create(email: params[:user][:email], password: params[:user][:password])
+      session[:id] = @user.id
+      redirect '/account'
+    end
+    erb :error
   end
 
   post '/login' do
-    @user = User.find_by(:email => params[:user][:email])
+    @user = User.find_by(email: params[:user][:email])
     if @user && @user.authenticate(params[:user][:password])
       session[:id] = @user.id
       redirect to '/account'
